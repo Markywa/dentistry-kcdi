c67f444eee0fa601733b0babea83ddc413efb83f
Markywa <mark0_baranov@mail.ru>
2024-06-15 13:57:46 +0700

    fix: исправлены урлы для прода

--- src/environments/urls.ts
+++ src/environments/urls.ts
@@ -1,5 +1,5 @@
 import { environment } from "./environments";
 
-export const API_SPECIALISTS ='api/website/specialists/' as const;
-export const API_CAROUSEL_SPECIALISTS = 'api/website/carousel-specialists/' as const;
-export const API_NOTIFICATION = 'api/website/notification/'as const;
+export const API_SPECIALISTS = `${environment.baseUrl}api/website/specialists/` as const;
+export const API_CAROUSEL_SPECIALISTS = `${environment.baseUrl}api/website/carousel-specialists/` as const;
+export const API_NOTIFICATION = `${environment.baseUrl}api/website/notification/` as const;
