d0b8d88281012cd4afb5b74a643ca815f4a32d01
Markywa <mark0_baranov@mail.ru>
2025-07-24 10:27:42 +0700

    feat: добавлено раскрытие отзывов

--- src/app/components/reviews-list/reviews-card/reviews-card.component.ts
+++ src/app/components/reviews-list/reviews-card/reviews-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input } from "@angular/core";
 import { ReviewData } from "../../../services/reviews/interfaces/review-data.interface";
 
 @Component({
@@ -10,8 +10,21 @@ import { ReviewData } from "../../../services/reviews/interfaces/review-data.int
 
 export class ReviewsCardComponent {
    @Input() reviewsCard!: ReviewData;
+   private cdr = inject(ChangeDetectorRef);
 
    get ratingArray(): number[] {
        return new Array(this.reviewsCard.rating);
+   }
+
+   isShowAll: boolean = false;
+
+   showAll(): void {
+    this.isShowAll = true;
+    this.cdr.markForCheck();
+   }
+
+   hideAll(): void{ 
+    this.isShowAll = false;
+    this.cdr.markForCheck();
    }
 }
